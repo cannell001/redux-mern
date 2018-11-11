a0ae8c45d023d35102f89baa6813daefd8e1ded2
Jeremy Cannell <jcannell007@hotmail.com>
2018-11-10 20:29:42 -0500

    adding to static data through componrent level state

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,57 +1,39 @@
-import React, { Component } from 'react';
-import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { connect } from 'react-redux';
-import { getItems, deleteItem } from '../actions/itemActions';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import uuid from "uuid";
 
 class ShoppingList extends Component {
-  componentDidMount() {
-    this.props.getItems();
-  }
-
-  onDeleteClick = id => {
-    this.props.deleteItem(id);
+  state = {
+    items: [
+      { id: uuid(), name: "Eggs" },
+      { id: uuid(), name: "Milk" },
+      { id: uuid(), name: "Steak" },
+      { id: uuid(), name: "Water" }
+    ]
   };
 
   render() {
-    const { items } = this.props.item;
+    const { items } = this.state; //destructuring
     return (
       <Container>
-        <ListGroup>
-          <TransitionGroup className="shopping-list">
-            {items.map(({ _id, name }) => (
-              <CSSTransition key={_id} timeout={500} classNames="fade">
-                <ListGroupItem>
-                  <Button
-                    className="remove-btn"
-                    color="danger"
-                    size="sm"
-                    onClick={this.onDeleteClick.bind(this, _id)}
-                  >
-                    &times;
-                  </Button>
-                  {name}
-                </ListGroupItem>
-              </CSSTransition>
-            ))}
-          </TransitionGroup>
-        </ListGroup>
+        <Button
+          color="dark"
+          style={{ marginBottom: "2rem" }}
+          onClick={() => {
+            const name = prompt("Enter Item");
+            if (name) {
+              this.setState(state => ({
+                items: [...state.items, { id: uuid(), name }]
+              }));
+            }
+          }}
+        >
+          Add Item
+        </Button>
       </Container>
     );
   }
 }
 
-ShoppingList.propTypes = {
-  getItems: PropTypes.func.isRequired,
-  item: PropTypes.object.isRequired
-};
-
-const mapStateToProps = state => ({
-  item: state.item
-});
-
-export default connect(
-  mapStateToProps,
-  { getItems, deleteItem }
-)(ShoppingList);
+export default ShoppingList;
