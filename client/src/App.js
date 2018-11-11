53ecfb4a4d690c3c3e65339d7b6bfc1f6c17d298
Jeremy Cannell <jcannell007@hotmail.com>
2018-11-11 09:40:35 -0500

    Create Store & Provider to share state with all components

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,25 @@ import ShoppingList from "./components/ShoppingList";
 //import ItemModal from "./components/ItemModal";
 import { Container } from "reactstrap";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <Container>
-          {/*<ItemModal />*/}
-          <ShoppingList />
-        </Container>
-      </div>
+      <Provider store={store}>
+        //share state throughout components
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            {/*<ItemModal />*/}
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
     );
   }
 }
